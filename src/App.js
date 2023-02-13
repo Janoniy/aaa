2418eb7e4f023a718b6664396e14a5208b91d913
Davron <janoniy7766@gmail.com>
2023-02-13 20:40:54 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return ( 
+    
+    <div className="container">
+        <header className="header">
+            <div className="Wrapper">
+            
+            <div className="flex">
+                  
+                  <div>
+                    <img src='https://cdn-icons-png.flaticon.com/512/72/72626.png'>
+                    </img>
+                  </div>
+
+
+                    <div className='nav_menu'>
+                      <ul>
+                        <li>
+                          <a href="">Home</a>
+                        </li>
+
+                        <li>
+                          <a href="">Profile</a>
+                        </li>
+                        <li>
+                          <a href="">Location</a>
+                        </li>
+
+                        <li>
+                          <a href="">Menu</a>
+                        </li>
+
+                        <li>
+                          <a href="">Faq</a>
+                        </li>
+                      </ul>
+                    </div>
+                </div>  
+            </div>
+          </header>
+
+              <nav className='nav'>
+                  <div className="wrapper_nav">
+                      <ul>
+                        <li>
+                          <a href="">Home</a>
+                        </li>
+
+                        <li>
+                          <a href="">Profile</a>
+                        </li>
+                        <li>
+                          <a href="">Location</a>
+                        </li>
+
+                        <li>
+                          <a href="">Menu</a>
+                        </li>
+
+                        <li>
+                          <a href="">Faq</a>
+                        </li>
+                      </ul>
+                    </div>
+                </nav>
+
+              <div className='content'>
+                  <img src='https://www.catenon.com/front/img/backgrounds/bg-digital.jpg'></img>
+              </div>
+              </div>
+     
+  );
+}
+
+export default App;
